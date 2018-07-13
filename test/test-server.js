6a9d8263627e47a39f168cec73fef7df59e4c703
Maria Palacios <imarip09@gmail.com>
2018-07-13 17:10:11 -0500

    added get test

---
+++ test/test-server.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, runServer, closeServer } = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('BlogPosts', function() {
+    before(function() {
+        return runServer;
+    });
+    after(function() {
+        return closeServer;
+    });
+
+    it('should return blog post on GET', function() {
+        return chai
+        .request(app)
+        .get('/blog-post')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('array');
+            expect(res.body.length).to.be.at.least(1);
+
+            const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+            res.body.forEach(function(item) {
+                expect(item).to.be.a('object')
+                expect(item).to.include.keys(expectedKeys)
+            });
+        });
+    });
+
+
+});
